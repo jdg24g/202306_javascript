1cf5566bb20cd545ba72f264b36f6b54a4a1cde5
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-06-20 23:18:07 -0400

    fin

--- Dojonary/src/script.js
+++ Dojonary/src/script.js
@@ -5,5 +5,27 @@ loginBtn.addEventListener("click", () => {
     loginBtn.innerText = "Logout";
   } else {
     loginBtn.innerText = "Login";
+    hideBtn.style.display = "block";
   }
 });
+
+const hideBtn = document.getElementById("definition");
+
+hideBtn.addEventListener("click", () => {
+  hideBtn.style.display = "none";
+});
+
+var contadorElemento = document.getElementById("contador");
+var contador = parseInt(contadorElemento.innerHTML);
+
+function masUno() {
+  contador++;
+  console.log(contador);
+  if (contador > 18) {
+    contador = 17;
+    alert("Ninja was disliked");
+  } else {
+    alert("Ninja was liked");
+  }
+  contadorElemento.innerHTML = contador;
+}
