73693cbc14aeb3b110726fd306cb3a580bb86cd4
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-06-22 18:15:29 -0400

    local storage aplicado

--- CORE-LIKES/src/script.js
+++ CORE-LIKES/src/script.js
@@ -1,18 +1,25 @@
 const tarjetas = document.querySelectorAll(".tarjeta");
 const conteosLikes = [];
 
+for (let i = 0; i < tarjetas.length; i++) {
+  const storedLikes = localStorage.getItem(`likes_${i}`);
+  conteosLikes[i] = storedLikes ? parseInt(storedLikes) : 0;
+}
+
 tarjetas.forEach((tarjeta, numero) => {
   const btnLike = tarjeta.querySelector(".btn");
+  const contadorLike = tarjeta.querySelector(".like");
 
-  conteosLikes[numero] = 0;
+  contadorLike.textContent = `${conteosLikes[numero]} Like(s)`;
 
   btnLike.addEventListener("click", () => {
     conteosLikes[numero]++;
-    const contadorLike = tarjeta.querySelector(".like");
     contadorLike.textContent = `${conteosLikes[numero]} Like(s)`;
-    //esto es para que no se pueda dar mas de 50 likes a la misma tarjeta
+    localStorage.setItem(`likes_${numero}`, conteosLikes[numero]);
+
     if (conteosLikes[numero] === 50) {
       conteosLikes[numero] = 0;
+      localStorage.setItem(`likes_${numero}`, 0);
     }
   });
 });
