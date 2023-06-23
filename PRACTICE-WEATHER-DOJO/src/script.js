ce25ec6271ec939b778644edc0bf79374f910550
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-06-23 15:04:00 -0400

    none

--- PRACTICE-WEATHER-DOJO/src/script.js
+++ PRACTICE-WEATHER-DOJO/src/script.js
@@ -1,11 +1,10 @@
-// Obtenemos los elementos de enlace
 const enlacesCiudad = document.querySelectorAll(".ciudad");
 console.log(enlacesCiudad[2].textContent);
 
 
 enlacesCiudad.forEach(function (enlace) {
   enlace.addEventListener("click", function (evento) {
-    evento.preventDefault(); // Evitamos la acción predeterminada del enlace
+    evento.preventDefault(); 
     const nombreCiudad = enlace.textContent; 
 
     const h3Elemento = document.querySelector(".subheader h3");
