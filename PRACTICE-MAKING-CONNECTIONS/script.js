c88729712130d97da0dbbf6ffac2804584af5df4
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-06-23 21:01:00 -0400

    FIX- LINEA22

--- PRACTICE-MAKING-CONNECTIONS/script.js
+++ PRACTICE-MAKING-CONNECTIONS/script.js
@@ -15,30 +15,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
   var elementosSolicitudConexion = document.querySelectorAll(".card-list-item");
 
-  elementosSolicitudConexion.forEach(function (item) {
-    var botonAceptar = item.querySelector(".icon.accept");
-    var botonCerrar = item.querySelector(".icon.no-accept");
+  if (elementosSolicitudConexion) {
+    elementosSolicitudConexion.forEach(function (item) {
+      var botonAceptar = item.querySelector(".icon.accept");
+      var botonCerrar = item.querySelector(".icon.no-accept");
+
+      if (botonAceptar && botonCerrar) {
+        botonAceptar.addEventListener("click", function () {
+          item.remove();
+          aumentarValorInsignia();
+          var addicionar = document.getElementById("badge-connection");
+          addicionar.textContent = parseInt(addicionar.textContent) + 1;
+        });
+
+        botonCerrar.addEventListener("click", function () {
+          item.remove();
+          aumentarValorInsignia();
+        });
+      }
+    });
+  }
+
+  function aumentarValorInsignia() {
     var elementoInsignia = document.querySelector(".card-header .badge");
+    
+    if (elementoInsignia) {
+      elementoInsignia.textContent = parseInt(elementoInsignia.textContent) - 1;
 
-    botonAceptar.addEventListener("click", function () {
-      item.remove();
-      aumentarValorInsignia(elementoInsignia);
-      var addicionar = document.getElementById("badge-connection");
-      addicionar.textContent = parseInt(addicionar.textContent) + 1;
-    });
-
-    botonCerrar.addEventListener("click", function () {
-      item.remove();
-      aumentarValorInsignia(elementoInsignia);
-    });
-  });
-
-  function aumentarValorInsignia(elementoInsignia) {
-    elementoInsignia.textContent = parseInt(elementoInsignia.textContent) - 1;
-
-    if (elementoInsignia.textContent === "0") {
-      var conexion = document.getElementById("connection");
-      conexion.remove();
+      if (elementoInsignia.textContent === "0") {
+        var conexion = document.getElementById("connection");
+        if (conexion) {
+          conexion.remove();
+        }
+      }
     }
   }
 });
